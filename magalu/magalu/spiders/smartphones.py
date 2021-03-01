b840cea64e6a947f10612201684d0f16f6a5417d
laisy <laisyferreira2011@gmail.com>
2021-02-28 23:26:48 -0300

    Paginaçao OK

--- magalu/magalu/spiders/smartphones.py
+++ magalu/magalu/spiders/smartphones.py
@@ -1,14 +1,17 @@
+# import do scrapy para usar as classes que o pacote fornece.
 import scrapy
 
 from scrapy import Request
 
 from magalu.items import MagaluItem
 
+# a partir a classe Spider, crio uma subclasse SmartphonesSpider
+# possui métodos e comportamentos que definem como seguir URLs e extrair dados das páginas que encontrar para saber onde procurar e quais dados procurar. 
 class SmartphonesSpider(scrapy.Spider):
     name = 'smartphones'
     allowed_domains = ['magazineluiza.com.br']
-
-    url = 'https://www.magazineluiza.com.br/smartphone/celulares-e-smartphones/s/te/tcsp/?page={page}'
+    base_url = 'https://www.magazineluiza.com.br'
+    url = 'https://www.magazineluiza.com.br/smartphone/celulares-e-smartphones/s/te/tcsp?page={page}'
 
     def start_requests(self):
         yield Request(self.url.format(page=1), callback=self.parse)
@@ -23,12 +26,22 @@ class SmartphonesSpider(scrapy.Spider):
             div = product.xpath("./div")[2]
             item["name"] = div.xpath("./h3/text()").get()
 
-            price_selector = div.xpath("./div")[1].xpath("./div")[1]
-            item["price"] = price_selector.xpath("./text()").get()
+            try:
+                price_selector = div.xpath("./div")[1].xpath("./div")[1]
+                item["price"] = price_selector.xpath("./text()").get()
+            
+            except IndexError:
+                price_selector = div.xpath("./div").xpath("./div")[1]
+                item["price"] = price_selector.xpath("./span").xpath("./text()").get()
 
             if not item["price"]:
-                item["price"] = price_selector.xpath("./span")[0].xpath("./text()").get()
+                item["price"] = price_selector.xpath("./span").xpath("./text()").get()
 
-            item["price"] = float(item["price"].split()[1].replace(",", ""))
+            if item["price"] != None:
+                item["price"] = float(item["price"].split()[1].replace(",", ""))
 
             yield item
+
+            next_page = response.xpath("//link[@rel='next']/@href").get()
+            if next_page:
+                yield Request(self.base_url + next_page, callback=self.parse)
